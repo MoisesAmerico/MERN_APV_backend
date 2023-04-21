de93a0338d558f2e705e0e5b1bb251a588758e8d
MoisesAmerico <cordovapacherresm@gmail.com>
2023-04-20 22:12:22 -0500

    deployment

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+const router = express.Router();
+import {
+  registrar,  
+  perfil,
+  confirmar,  
+  autenticar,  
+  olvidePassword,  
+  comprobarToken,
+  nuevoPassword,
+  actualizarPerfil,
+  actualizarPassword,
+  
+} from "../controllers/veterinarioController.js";
+import checkAuth from "../middleware/authMiddleware.js";
+
+// área publica
+router.post("/", registrar);
+router.get("/confirmar/:token", confirmar);
+router.post("/login", autenticar);
+router.post("/olvide-password", olvidePassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+//La inea 24 es una mejor forma de hacer:
+//router.get("/olvide-password/:token", comprobarToken);
+//router.post("/olvide-password/:token", nuevoPassword);
+
+// Area privada
+router.get("/perfil", checkAuth, perfil);
+router.put("/perfil/:id", checkAuth, actualizarPerfil);
+router.put("/actualizar-password", checkAuth, actualizarPassword);
+
+
+export default router;
